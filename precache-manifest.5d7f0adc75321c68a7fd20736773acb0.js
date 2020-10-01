a1524858522cea7c7c3863d8c6501e4307e9a352
zina kamel <zena.kamel100@gmail.com>
2020-10-01 09:52:56 +0300

    Updates

---
+++ precache-manifest.5d7f0adc75321c68a7fd20736773acb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02a8df2a50ce7cb2ea0f787ceae8df9",
+    "url": "/hiking_api/index.html"
+  },
+  {
+    "revision": "e517f213274848fc3f4b",
+    "url": "/hiking_api/static/css/main.07e6a917.chunk.css"
+  },
+  {
+    "revision": "c830348588edc99450e0",
+    "url": "/hiking_api/static/js/2.257b8084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hiking_api/static/js/2.257b8084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e517f213274848fc3f4b",
+    "url": "/hiking_api/static/js/main.4d228a01.chunk.js"
+  },
+  {
+    "revision": "08cc7fea42fb6c14191b",
+    "url": "/hiking_api/static/js/runtime-main.71211480.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/hiking_api/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "2999bd94818efac7a8a1602c52c90c9b",
+    "url": "/hiking_api/static/media/bllogo.2999bd94.PNG"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/hiking_api/static/media/spinner.235b9b3e.gif"
+  }
+]);
